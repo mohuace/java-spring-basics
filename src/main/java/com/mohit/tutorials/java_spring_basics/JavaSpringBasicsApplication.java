a9b574602759cf8c2a362c87ab0773b0854a4218
mohit98thaker <mohuace@gmail.com>
2025-02-24 21:55:09 -0500

    Initial commit

---
+++ src/main/java/com/mohit/tutorials/java_spring_basics/JavaSpringBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.tutorials.java_spring_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohit/tutorials/java_spring_basics/JavaSpringBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.tutorials.java_spring_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
